29c63050333b0210117fb76012b05d589c745771
KenAgudelo <juan.agudelo046@pascualbravo.edu.co>
2025-08-26 13:00:02 -0500

    Error Fix

--- Pantallas_Sistema_facturacion/FrmEditarCategoria.Designer.cs
+++ Pantallas_Sistema_facturacion/FrmEditarCategoria.Designer.cs
@@ -32,7 +32,6 @@
             txtID = new TextBox();
             btnSalir = new ReaLTaiizor.Controls.SpaceButton();
             btnActualizar = new ReaLTaiizor.Controls.SpaceButton();
-            txtNombreCategoria = new ReaLTaiizor.Controls.MaterialMaskedTextBox();
             SuspendLayout();
             // 
             // lblTitulo
@@ -60,7 +59,7 @@
             btnSalir.Customization = "Kioq/zIyMv8yMjL/Kioq/y8vL/8nJyf//v7+/yMjI/8qKir/";
             btnSalir.Font = new Font("Verdana", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             btnSalir.Image = null;
-            btnSalir.Location = new Point(237, 214);
+            btnSalir.Location = new Point(237, 294);
             btnSalir.Name = "btnSalir";
             btnSalir.NoRounding = false;
             btnSalir.Size = new Size(171, 50);
@@ -75,7 +74,7 @@
             btnActualizar.Customization = "Kioq/zIyMv8yMjL/Kioq/y8vL/8nJyf//v7+/yMjI/8qKir/";
             btnActualizar.Font = new Font("Verdana", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             btnActualizar.Image = null;
-            btnActualizar.Location = new Point(42, 214);
+            btnActualizar.Location = new Point(42, 294);
             btnActualizar.Name = "btnActualizar";
             btnActualizar.NoRounding = false;
             btnActualizar.Size = new Size(171, 50);
@@ -85,56 +84,15 @@
             btnActualizar.Transparent = false;
             btnActualizar.Click += btnActualizar_Click;
             // 
-            // txtNombreCategoria
-            // 
-            txtNombreCategoria.AllowPromptAsInput = true;
-            txtNombreCategoria.AnimateReadOnly = false;
-            txtNombreCategoria.AsciiOnly = false;
-            txtNombreCategoria.BackgroundImageLayout = ImageLayout.None;
-            txtNombreCategoria.BeepOnError = false;
-            txtNombreCategoria.CutCopyMaskFormat = MaskFormat.IncludeLiterals;
-            txtNombreCategoria.Depth = 0;
-            txtNombreCategoria.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
-            txtNombreCategoria.HidePromptOnLeave = false;
-            txtNombreCategoria.HideSelection = true;
-            txtNombreCategoria.Hint = "Nombre Categoria";
-            txtNombreCategoria.InsertKeyMode = InsertKeyMode.Default;
-            txtNombreCategoria.LeadingIcon = null;
-            txtNombreCategoria.Location = new Point(42, 139);
-            txtNombreCategoria.Mask = "";
-            txtNombreCategoria.MaxLength = 32767;
-            txtNombreCategoria.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.OUT;
-            txtNombreCategoria.Name = "txtNombreCategoria";
-            txtNombreCategoria.PasswordChar = '\0';
-            txtNombreCategoria.PrefixSuffixText = null;
-            txtNombreCategoria.PromptChar = '_';
-            txtNombreCategoria.ReadOnly = false;
-            txtNombreCategoria.RejectInputOnFirstFailure = false;
-            txtNombreCategoria.ResetOnPrompt = true;
-            txtNombreCategoria.ResetOnSpace = true;
-            txtNombreCategoria.RightToLeft = RightToLeft.No;
-            txtNombreCategoria.SelectedText = "";
-            txtNombreCategoria.SelectionLength = 0;
-            txtNombreCategoria.SelectionStart = 0;
-            txtNombreCategoria.ShortcutsEnabled = true;
-            txtNombreCategoria.Size = new Size(366, 48);
-            txtNombreCategoria.SkipLiterals = true;
-            txtNombreCategoria.TabIndex = 10;
-            txtNombreCategoria.TabStop = false;
-            txtNombreCategoria.TextAlign = HorizontalAlignment.Left;
-            txtNombreCategoria.TextMaskFormat = MaskFormat.IncludeLiterals;
-            txtNombreCategoria.TrailingIcon = null;
-            txtNombreCategoria.UseSystemPasswordChar = false;
-            txtNombreCategoria.ValidatingType = null;
+         
             // 
             // FrmEditarCategoria
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(450, 320);
+            ClientSize = new Size(451, 377);
             Controls.Add(btnSalir);
             Controls.Add(btnActualizar);
-            Controls.Add(txtNombreCategoria);
             Controls.Add(txtID);
             Controls.Add(lblTitulo);
             FormBorderStyle = FormBorderStyle.None;
@@ -152,6 +110,5 @@
         private TextBox txtID;
         private ReaLTaiizor.Controls.SpaceButton btnSalir;
         private ReaLTaiizor.Controls.SpaceButton btnActualizar;
-        private ReaLTaiizor.Controls.MaterialMaskedTextBox txtNombreCategoria;
     }
 }
